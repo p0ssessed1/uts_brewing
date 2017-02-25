e1e9f64d5c20200ab62001aa3bc1305f8de801e8
sbrennan970 <sbrennan970@gmail.com>
2017-02-25 17:16:31 +0000

    Added Loops for read & write

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,31 @@
+int thermistor = 2;
+int heatbelt = 4;
+unsigned long startime=0;
+unsigned long stoptime=0;
+unsigned long ontime=0;
 void setup() {
   // put your setup code here, to run once:
-
+  pinmode (thermistor, INPUT);
+  pinmode (heatbelt, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+ 
+  if (bitRead(PORTD,heatbelt) == HIGH)
+    {
+      if (digitalRead(thermistor)>20)
+        {
+          digitalWrite(heatbelt, LOW);
+          stoptime=millis();
+        }
+      
+    }
+  else if (digitalREAD(thermistor)<17)
+    {
+      digitalWrite(heatbelt, HIGH);
+      startime = millis();
+    }
+   
+ ontime = ontime + (stoptime-startime);  
+ delay(300000);    
 }
