06e0961a3e0a43725b1f7f7e5bd590ba5b533cca
Stephen Brennan <Stephen Brennan>
2017-02-25 17:12:39 +0000

    Initialize WiFi

--- main/main.ino
+++ main/main.ino
@@ -1,6 +1,22 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiServer.h>
+#include <WiFiUdp.h>
+
+#include "uts_wifi.h"
+
+char ssid[] = "SSID";
+char pass[] = "PassWord";
+
 void setup() {
   // put your setup code here, to run once:
+  // initialize serial:
+  Serial.begin(9600);
+  uts_wifi wifi = uts_wifi(ssid,pass);
 
+  if(WL_CONNECTED != wifi.uts_wifi_setup()) {
+    Serial.println("Error connecting to WiFi");
+  }
 }
 
 void loop() {
--- main/uts_wifi.h
+++ main/uts_wifi.h
@@ -4,22 +4,22 @@
 	
 */
 	
-#ifndef uts_wifi_h
-#define uts_wifi_h
+#ifndef UTS_WIFI_H
+#define UTS_WIFI_H
 
 #include "Arduino.h"
 #include <Wifi.h>
 
+#define MAX_WPA_PASS_NUM (12)
+#define MAX_SSID_NUM (10)
 class uts_wifi
 {
 	public:
-		uts_wifi(string ssid, string password);
+		uts_wifi(char *ssid, char *password);
 		int uts_wifi_setup();	
 	private:
-		string _ssid;
-		string _password;
-			
-		
+		char *_ssid;
+		char *_password;
 };
 
-
+#endif /* UTS_WIFI_H */
